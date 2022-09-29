804d77a085a7830bd838e071260b68d6a706777d
TornadoMC <42751295+TornadoMC2@users.noreply.github.com>
2022-09-29 09:10:27 -0500

    refactor some

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,23 +1,26 @@
 const mongoose = require('mongoose');
 
-
+const { deployCommands } = require('./utils/deployCommands')
 
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
 
-        await mongoose.connect(
-            process.env.MONGO_CLIENT,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                autoIndex: false
-            },
-            (err) => {
-                if (err) return console.error(err);
-            }
-        )
+        // await mongoose.connect(
+        //     process.env.MONGO_CLIENT,
+        //     {
+        //         useNewUrlParser: true,
+        //         useUnifiedTopology: true,
+        //         autoIndex: false
+        //     },
+        //     (err) => {
+        //         if (err) return console.error(err);
+        //     }
+        // )
+
+        console.log('Deploying Commands')
+        deployCommands();
 
         console.log(`Ready! Logged in as ${client.user.tag}`);
     },
